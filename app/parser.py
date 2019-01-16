eb62f1394621fcfc543f21dc943641c06bcb0edc
seongwonhan88 <seongwonhan88@gmail.com>
2019-01-16 23:32:23 +0900

    scrape post from linkedin

--- app/parser.py
+++ app/parser.py
@@ -1,11 +1,17 @@
-import os
-
 import requests
 from bs4 import BeautifulSoup
 
 from config.settings import session
 
-
 with requests.Session() as s:
     login_request = s.post("https://www.linkedin.com/uas/login-submit?loginSubmitSource=GUEST_HOME", data=session)
     print(login_request.status_code)
+
+    if login_request.status_code != 200:
+        raise Exception('login failed!')
+    feed = s.get('https://www.linkedin.com/feed/')
+    soup = BeautifulSoup(feed.text, 'html.parser')
+    title = soup.select('#ember71 > h3 > span > span')
+    contents = soup.select('#ember101 > span')
+
+    print(title)
