298de9cd329d704208601dc3d87e1229a8229534
seongwonhan88 <seongwonhan88@gmail.com>
2019-01-17 22:59:28 +0900

    selenium test finished

--- app/parser.py
+++ app/parser.py
@@ -1,17 +1,15 @@
-import requests
+from selenium import webdriver
+from config.settings import session
 from bs4 import BeautifulSoup
 
-from config.settings import session
+driver = webdriver.Chrome('/Users/seongwon/Downloads/chromedriver')
+driver.implicitly_wait(3)
+driver.get('https://linkedin.com')
+driver.find_element_by_id('login-email').send_keys(session['session_key'])
+driver.find_element_by_id('login-password').send_keys(session['session_password'])
+driver.find_element_by_id('login-submit').click()
 
-with requests.Session() as s:
-    login_request = s.post("https://www.linkedin.com/uas/login-submit?loginSubmitSource=GUEST_HOME", data=session)
-    print(login_request.status_code)
-
-    if login_request.status_code != 200:
-        raise Exception('login failed!')
-    feed = s.get('https://www.linkedin.com/feed/')
-    soup = BeautifulSoup(feed.text, 'html.parser')
-    title = soup.select('#ember71 > h3 > span > span')
-    contents = soup.select('#ember101 > span')
-
-    print(title)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+title = soup.select('div.relative')
+print(title)
