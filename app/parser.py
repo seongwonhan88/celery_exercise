45efac67c44875143851d7062371b991b6f12474
seongwonhan88 <seongwonhan88@gmail.com>
2019-01-21 23:32:44 +0900

    parser.py changed to google search

--- app/parser.py
+++ app/parser.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
-from config.settings import session
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome('/Users/seongwon/Downloads/chromedriver')
 driver.implicitly_wait(3)
-driver.get('https://linkedin.com')
-driver.find_element_by_id('login-email').send_keys(session['session_key'])
-driver.find_element_by_id('login-password').send_keys(session['session_password'])
-driver.find_element_by_id('login-submit').click()
+driver.get('https://www.google.com')
+
+driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input').send_keys('cafe')
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div[2]/div/div[4]/div[2]/div/div/a').click()
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-title = soup.select('div.relative')
-print(title)
+
